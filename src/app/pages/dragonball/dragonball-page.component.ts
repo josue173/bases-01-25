2d835abff7410a140836336a0932c98f79c77734
josue173 <josue.perez04@outlook.com>
2025-08-24 20:46:25 -0600

    feat: new for

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
   imports: [],
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballComponent {}
+export class DragonballComponent {
+  public characters = signal<Character[]>([
+    {id: 1, name: 'Hola1', power: 901},
+    {id: 2, name: 'Hola2', power: 801},
+    {id: 3, name: 'Hola3', power: 701}
+  ]);
+
+}
